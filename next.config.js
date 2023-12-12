058e5c560a12643962e29cd8f813112594836af2
Nabil El Maaroufi <93434382+nabilelmaa@users.noreply.github.com>
2023-12-12 20:31:52 +0100

    ignone build errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["cdn.imagin.studio"],
-  },
-  typescription: {
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
+    images: {
+      domains: ["cdn.imagin.studio"],
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
